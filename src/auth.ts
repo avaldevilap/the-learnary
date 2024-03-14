c2a5685de2c4008d394ab721df74acef583b08ff
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2024-03-14 20:55:21 +0100

    refactor: Improve code formatting in GET endpoint
    Refactor GET endpoint code to improve readability and maintain consistency in formatting. Changes include aligning indentation for better code structure and clarity. No functional changes made, only code formatting improvements.

--- src/auth.ts
+++ src/auth.ts
@@ -7,31 +7,31 @@ import { Lucia } from "lucia";
 console.log("DB_URL", dbUrl);
 
 const db = new Database(
-	// "/Users/avaldevilap/Documents/GitHub/the-learnary/.astro/content.db",
-	dbUrl,
+  // "/Users/avaldevilap/Documents/GitHub/the-learnary/.astro/content.db",
+  dbUrl,
 );
 
 const adapter = new BunSQLiteAdapter(db, {
-	user: "user",
-	session: "session",
+  user: "user",
+  session: "session",
 });
 
 export const lucia = new Lucia(adapter, {
-	sessionCookie: {
-		attributes: {
-			// set to `true` when using HTTPS
-			secure: import.meta.env.PROD,
-		},
-	},
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: import.meta.env.PROD,
+    },
+  },
 });
 
 export const github = new GitHub(
-	import.meta.env.GITHUB_CLIENT_ID,
-	import.meta.env.GITHUB_CLIENT_SECRET,
+  import.meta.env.GITHUB_CLIENT_ID,
+  import.meta.env.GITHUB_CLIENT_SECRET,
 );
 
 declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-	}
+  interface Register {
+    Lucia: typeof lucia;
+  }
 }
