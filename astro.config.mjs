385894151184bf233d5f6ff65e0c1984dc94ea97
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2024-03-14 20:17:01 +0100

    refactor: Updated scripts in package.json for better consistency and readability
    feat: Added new dependencies and updated existing ones for improved functionality
    feat: Updated database schema with new columns for enhanced user authentication capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
-
 import db from "@astrojs/db";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "standalone",
-	}),
-	integrations: [simpleStackForm(), db()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [simpleStackForm(), db(), sentry(), spotlightjs()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- drizzle/0003_cultured_karnak.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "content" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "course" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "enrollment" ALTER COLUMN "id" SET DATA TYPE serial;
